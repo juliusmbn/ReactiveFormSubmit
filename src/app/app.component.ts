bfab4f34dd92d6d2e3190dd1041188e61b5d0aff
juliusmbn <julius@mybizniche.com>
2017-06-26 12:15:49 +0800

    form update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {  FormBuilder, FormGroup, Validators  } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  employeeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { // <--- inject FormBuilder
+    this.createForm();
+  }
+
+   createForm() {
+    this.employeeForm = this.fb.group({
+      firstName: ['', Validators.required ], 
+      lastName: ['', Validators.required ]
+    });
+  }
+
+  onSubmit(){
+   console.log(this.employeeForm.value, this.employeeForm.valid);
+  }
+  
 }
